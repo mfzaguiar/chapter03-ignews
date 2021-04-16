8d32d10138808f65b2a1f8a101ab35dba10aa44b
Matheus Aguiar <first_mf@hotmail.com>
2021-04-16 15:40:16 -0300

    add Header component

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ignews - Início</title>
+      </Head>
+      <h1>Ola mundo</h1>
+    </>
+  )
+}
