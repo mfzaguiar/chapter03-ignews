a28b6cc64b3a663ab92e10af18c5f562aa4831ef
Matheus Aguiar <first_mf@hotmail.com>
2021-04-16 22:16:34 -0300

    add github authentication using next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async() => {
+export const getStaticProps: GetStaticProps = async() => {
   const price = await stripe.prices.retrieve('price_1IgxTFAXpzzSbic0gACHTTkQ')
 
   const product = {
@@ -46,12 +46,13 @@ export const getServerSideProps: GetServerSideProps = async() => {
     amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD'
-    }).format(price.unit_amount/100),
+    }).format(price.unit_amount / 100),
   }
   
   return {
     props : {
      product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
