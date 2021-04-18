23ed32723bebfb7206bdd99b0e12a544a87a42aa
Matheus Aguiar <first_mf@hotmail.com>
2021-04-17 21:55:51 -0300

    config stripe webhooks

---
+++ src/pages/api/_lib/menageSubscription.ts
@@ -0,0 +1,59 @@
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
+import { stripe } from '../../../services/stripe';
+
+export async function saveSubscription(
+    subscriptionId: string,
+    customerId: string,
+    createAction = false,
+) {
+    const userRef = await fauna.query(
+      q.Select(
+        "ref",
+        q.Get(
+            q.Match(
+                q.Index('user_by_stripe_customer_id'),
+                customerId
+            )
+        )
+      )
+    )
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+    const subscriptionData ={
+        id: subscription.id,
+        userId: userRef,
+        status: subscription.status,
+        price_id: subscription.items.data[0].price.id,
+    }
+
+    // CRIAR NOVA SUBSCRIPTION 
+    if(createAction) {
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                { data: subscriptionData}
+            )
+        )
+    }else {
+        // ATUALIZAR SUBSCRIPTION
+        await fauna.query(
+            q.Replace(
+                q.Select(
+                    "ref",
+                    q.Get(
+                        q.Match(
+                            q.Index('subscription_by_id'),
+                            subscriptionId
+                        )
+                    )
+                ),
+               { data : subscriptionData} 
+            )
+        )
+    }
+    
+  // Buscar o usu[ario no banco do Faunadb com o ID striper customerId
+  // Salvar os dados da subscription no FaunaDB
+}
